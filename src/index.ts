1bb6f6c1064ac3e70fe4a413b4de7822402957b0
sahandzou <sahandzoufan79@gmail.com>
2022-03-26 11:03:32 +0430

    deleting comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import koiosNetwork from "./network/koios";
 import { main } from "./scanner/scanner";
 
-// koiosNetwork.getBlockAtHeight(100).then(res=>console.log(res))
-// koiosNetwork.getBlock().then(res=>console.log(res))
-// koiosNetwork.getBlockTxs("153b4df27d40a149182d1ea428a4d5d32355bd6df4f9892d18dc33c94fae3fb3").then(res=>console.log(res))
 main()
