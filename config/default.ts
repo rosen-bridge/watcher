6374b7ba33263b509b11a24ab50b5a3c675a09dd
sahandzou <sahandzoufan79@gmail.com>
2022-03-30 14:53:13 +0430

    set config interval

--- config/default.ts
+++ config/default.ts
@@ -1,10 +1,9 @@
 export default {
     node: {
         URL: "https://testnet.koios.rest/api/v0",
-        interval:2,
     },
     scanner: {
-        interval: 2,
+        interval: 10,
     }
 };
 
