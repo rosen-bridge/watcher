2327d690e3a065680c4585f88cf97bc3f2bfe92e
sahandzou <sahandzoufan79@gmail.com>
2022-04-08 13:41:41 +0430

    bug fix

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -3,7 +3,8 @@ import { DataSource } from "typeorm";
 export const ormconfig = new DataSource({
     type: "sqlite",
     database: "dir/sqlite/watcher.sqlite",
-    entities: ["dir/src/entities/*.js"],
+    entities: ["src/entities/*{.js,.ts}"],
+    // entities: ["dir/src/entities/*.js"], // production migration
     migrations: ["dir/src/migrations/*.js"],
     synchronize: false,
     logging: false,
--- src/scanner/scanner.ts
+++ src/scanner/scanner.ts
@@ -1,8 +1,9 @@
 import { KoiosNetwork } from "../network/koios";
-import { CardanoUtils } from "./utils";
+import { CardanoUtils, Observation } from "./utils";
 import config from "config";
 import DataBase from "./models";
 import { ormconfig } from "../../config/ormconfig";
+import { Block } from "../objects/apiModels";
 
 const INTERVAL: number | undefined = config.get?.('scanner.interval');
 
@@ -18,9 +19,26 @@ export class Scanner {
      * @return Promise<Boolean>
      */
     isForkHappen = async (): Promise<Boolean> => {
-        const lastSavedBlock = (await this._dataBase.getLastSavedBlock())!;
-        const lastSavedBlockFromNetwork = await KoiosNetwork.getBlockAtHeight(lastSavedBlock.block_height);
-        return lastSavedBlockFromNetwork.hash !== lastSavedBlock.hash;
+        const lastSavedBlock = await this._dataBase.getLastSavedBlock();
+        if (lastSavedBlock !== undefined) {
+            const lastSavedBlockFromNetwork = await KoiosNetwork.getBlockAtHeight(lastSavedBlock.block_height);
+            return lastSavedBlockFromNetwork.hash !== lastSavedBlock.hash;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * getting last block and observations from the network
+     * @param height
+     * @return Promise<[Block, Array<Observation | undefined>]>
+     */
+    getBlockAndObservations = async (height: number): Promise<[Block, Array<Observation | undefined>]> => {
+        const block = await KoiosNetwork.getBlockAtHeight(height);
+        const observations = (await CardanoUtils.observationsAtHeight(block.hash))
+            .filter((observation) => {
+                return observation !== undefined
+            });
+        return [block, observations];
     }
 
     /**
@@ -29,37 +47,35 @@ export class Scanner {
      */
     update = async () => {
         const lastSavedBlock = (await this._dataBase.getLastSavedBlock());
-        if (lastSavedBlock !== undefined) {
-            if (!await this.isForkHappen()) {
-                const lastBlockHeight = await KoiosNetwork.getBlock(0).then(res => {
-                    return res[0].block_height
-                });
-                if ("block_height" in lastSavedBlock) {
-                    for (let height = lastSavedBlock.block_height + 1; height <= lastBlockHeight; height++) {
-                        const block = await KoiosNetwork.getBlockAtHeight(height);
-                        const observations = (await CardanoUtils.observationsAtHeight(block.hash))
-                            .filter((observation) => {
-                                return observation !== undefined
-                            });
-                        if (!await this.isForkHappen()) {
-                            if (!await this._dataBase.saveBlock(block.block_height, block.hash, observations)) {
-                                break;
-                            }
-                        } else {
+        if (!await this.isForkHappen()) {
+            const lastBlockHeight = await KoiosNetwork.getBlock(0).then(res => {
+                return res[0].block_height
+            });
+            if (lastSavedBlock !== undefined) {
+                for (let height = lastSavedBlock.block_height + 1; height <= lastBlockHeight; height++) {
+                    const [block, observations] = await this.getBlockAndObservations(height);
+                    if (!await this.isForkHappen()) {
+                        if (!await this._dataBase.saveBlock(block.block_height, block.hash, observations)) {
                             break;
                         }
+                    } else {
+                        break;
                     }
                 }
             } else {
-                let forkPointer = lastSavedBlock!;
-                let blockFromNetwork = await KoiosNetwork.getBlockAtHeight(forkPointer.block_height);
-                while (blockFromNetwork.hash !== forkPointer.hash) {
-                    forkPointer = (await this._dataBase.getBlockAtHeight(forkPointer.block_height - 1))!;
-                    blockFromNetwork = await KoiosNetwork.getBlockAtHeight(blockFromNetwork.block_height - 1);
-                }
-                await this._dataBase.changeLastValidBlock(forkPointer.block_height);
+                const [block, observations] = await this.getBlockAndObservations(lastBlockHeight);
+                await this._dataBase.saveBlock(block.block_height, block.hash, observations);
             }
+        } else {
+            let forkPointer = lastSavedBlock!;
+            let blockFromNetwork = await KoiosNetwork.getBlockAtHeight(forkPointer.block_height);
+            while (blockFromNetwork.hash !== forkPointer.hash) {
+                forkPointer = (await this._dataBase.getBlockAtHeight(forkPointer.block_height - 1))!;
+                blockFromNetwork = await KoiosNetwork.getBlockAtHeight(blockFromNetwork.block_height - 1);
+            }
+            await this._dataBase.changeLastValidBlock(forkPointer.block_height);
         }
+
     }
 
 }
@@ -69,12 +85,12 @@ export class Scanner {
  */
 export const main = async () => {
     const DB = await DataBase.init(ormconfig);
-
     const scanner = new Scanner(DB);
     if (typeof INTERVAL === 'number') {
         setInterval(scanner.update, INTERVAL * 1000);
+        console.log("first iteration");
     } else {
         console.log("scanner interval doesn't set in the config");
     }
-      
+
 }
