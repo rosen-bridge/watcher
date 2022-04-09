144796b2b3064056e59dade649497d25fcd7967e
sahandzou <sahandzoufan79@gmail.com>
2022-04-09 12:54:48 +0430

    migration refactor

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -2,10 +2,10 @@ import { DataSource } from "typeorm";
 
 export const ormconfig = new DataSource({
     type: "sqlite",
-    database: "dir/sqlite/watcher.sqlite",
-    entities: ["src/entities/*{.js,.ts}"],
-    // entities: ["dir/src/entities/*.js"], // production migration
-    migrations: ["dir/src/migrations/*.js"],
+    database: __dirname + "/../../sqlite/watcher.sqlite",
+    entities: [__dirname + "/../src/entities/*{.js,.ts}"],
+    migrations: [__dirname + "/../src/migrations/*{.js,.ts}"],
     synchronize: false,
     logging: false,
-})
+});
+
