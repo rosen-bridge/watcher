b896dbf90a060dec88fbeebeb3e10837230d5c1b
sahandzou <sahandzoufan79@gmail.com>
2022-04-16 14:31:21 +0430

    adding scanner main

--- config/default.ts
+++ config/default.ts
@@ -4,7 +4,7 @@ export default {
     },
     scanner: {
         interval: 10,
-        initialBlockHeight: 3433333,
+        initialBlockHeight: 3471392,
     },
     addresses: {
         bank: ["addr_test1vze7yqqlg8cjlyhz7jzvsg0f3fhxpuu6m3llxrajfzqecggw704re"],
