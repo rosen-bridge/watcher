de7f8aa5047a0ac2d4bf14b7592c82f3409bd286
sahandzou <sahandzoufan79@gmail.com>
2022-04-25 19:24:00 +0430

    init express & ergo-lib-wasm

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import { main } from "./cardano/scanner/scanner";
 import "reflect-metadata";
+import express from "express";
 
-main()
+// main()
+const app = express();
+
+app.get("/secret", (req, res) => {
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`app listenning on port ${port}`));
+
