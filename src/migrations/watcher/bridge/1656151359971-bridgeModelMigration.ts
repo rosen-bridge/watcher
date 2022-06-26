831c66d79c7d0648ae9caf848d958bd71d71759e
Moein zargarzadeh <moein.zargarzadeh@gmail.com>
2022-06-26 11:16:57 +0430

    improve migrations

--- src/migrations/watcher/bridge/1656151359971-bridgeModelMigration.ts
+++ src/migrations/watcher/bridge/1656151359971-bridgeModelMigration.ts
@@ -4,28 +4,47 @@ export class bridgeModelMigration1656151359971 implements MigrationInterface {
     name = 'bridgeModelMigration1656151359971'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "bridge_block_entity" ("height" integer PRIMARY KEY NOT NULL, "hash" varchar(64) NOT NULL, CONSTRAINT "UQ_704f5c9eafd1fcabc287feb19cc" UNIQUE ("hash"))`);
-        await queryRunner.query(`CREATE TABLE "box_entity" ("id" integer PRIMARY KEY NOT NULL, "boxId" varchar NOT NULL, "value" bigint NOT NULL, "type" varchar CHECK( "type" IN ('permit','wid','plain') ) NOT NULL, "boxJson" varchar NOT NULL, "blockHeight" integer, "spendBlockHeight" integer)`);
-        await queryRunner.query(`CREATE TABLE "observed_commitment_entity" ("id" integer PRIMARY KEY NOT NULL, "eventId" varchar NOT NULL, "commitment" varchar NOT NULL, "WID" varchar NOT NULL, "commitmentBoxId" varchar NOT NULL, "eventTriggerBoxId" varchar, "blockHeight" integer, "spendBlockHeight" integer)`);
-        await queryRunner.query(`CREATE TABLE "temporary_box_entity" ("id" integer PRIMARY KEY NOT NULL, "boxId" varchar NOT NULL, "value" bigint NOT NULL, "type" varchar CHECK( "type" IN ('permit','wid','plain') ) NOT NULL, "boxJson" varchar NOT NULL, "blockHeight" integer, "spendBlockHeight" integer, CONSTRAINT "FK_cbd976955f2cce93fda0069865e" FOREIGN KEY ("blockHeight") REFERENCES "bridge_block_entity" ("height") ON DELETE CASCADE ON UPDATE NO ACTION, CONSTRAINT "FK_443356c00a735cbccb805f893d0" FOREIGN KEY ("spendBlockHeight") REFERENCES "bridge_block_entity" ("height") ON DELETE SET NULL ON UPDATE NO ACTION)`);
-        await queryRunner.query(`INSERT INTO "temporary_box_entity"("id", "boxId", "value", "type", "boxJson", "blockHeight", "spendBlockHeight") SELECT "id", "boxId", "value", "type", "boxJson", "blockHeight", "spendBlockHeight" FROM "box_entity"`);
-        await queryRunner.query(`DROP TABLE "box_entity"`);
-        await queryRunner.query(`ALTER TABLE "temporary_box_entity" RENAME TO "box_entity"`);
-        await queryRunner.query(`CREATE TABLE "temporary_observed_commitment_entity" ("id" integer PRIMARY KEY NOT NULL, "eventId" varchar NOT NULL, "commitment" varchar NOT NULL, "WID" varchar NOT NULL, "commitmentBoxId" varchar NOT NULL, "eventTriggerBoxId" varchar, "blockHeight" integer, "spendBlockHeight" integer, CONSTRAINT "FK_500f90c61fb27327ee25350c33b" FOREIGN KEY ("blockHeight") REFERENCES "bridge_block_entity" ("height") ON DELETE CASCADE ON UPDATE NO ACTION, CONSTRAINT "FK_e43db92f2466f82759cec509625" FOREIGN KEY ("spendBlockHeight") REFERENCES "bridge_block_entity" ("height") ON DELETE SET NULL ON UPDATE NO ACTION)`);
-        await queryRunner.query(`INSERT INTO "temporary_observed_commitment_entity"("id", "eventId", "commitment", "WID", "commitmentBoxId", "eventTriggerBoxId", "blockHeight", "spendBlockHeight") SELECT "id", "eventId", "commitment", "WID", "commitmentBoxId", "eventTriggerBoxId", "blockHeight", "spendBlockHeight" FROM "observed_commitment_entity"`);
-        await queryRunner.query(`DROP TABLE "observed_commitment_entity"`);
-        await queryRunner.query(`ALTER TABLE "temporary_observed_commitment_entity" RENAME TO "observed_commitment_entity"`);
+        await queryRunner.query(`CREATE TABLE "bridge_block_entity" 
+                (
+                    "height" integer PRIMARY KEY NOT NULL,
+                     "hash" varchar(64) NOT NULL,
+                     CONSTRAINT "UQ_704f5c9eafd1fcabc287feb19cc" UNIQUE ("hash")
+                )`
+        );
+        await queryRunner.query(`CREATE TABLE "box_entity" 
+                (
+                    "id" integer PRIMARY KEY NOT NULL,
+                    "boxId" varchar NOT NULL,
+                    "value" bigint NOT NULL,
+                    "type" varchar CHECK( "type" IN ('permit','wid','plain') ) NOT NULL,
+                    "boxJson" varchar NOT NULL,
+                    "blockHeight" integer,
+                    "spendBlockHeight" integer,
+                    CONSTRAINT "FK_cbd976955f2cce93fda0069865e" FOREIGN KEY ("blockHeight") 
+                        REFERENCES "bridge_block_entity" ("height") ON DELETE CASCADE ON UPDATE NO ACTION,
+                    CONSTRAINT "FK_443356c00a735cbccb805f893d0" FOREIGN KEY ("spendBlockHeight") 
+                        REFERENCES "bridge_block_entity" ("height") ON DELETE SET NULL ON UPDATE NO ACTION
+                )`
+        );
+        await queryRunner.query(`CREATE TABLE "observed_commitment_entity" 
+                (
+                    "id" integer PRIMARY KEY NOT NULL,
+                    "eventId" varchar NOT NULL,
+                    "commitment" varchar NOT NULL,
+                    "WID" varchar NOT NULL,
+                    "commitmentBoxId" varchar NOT NULL,
+                    "eventTriggerBoxId" varchar,
+                    "blockHeight" integer,
+                    "spendBlockHeight" integer,
+                    CONSTRAINT "FK_500f90c61fb27327ee25350c33b" FOREIGN KEY ("blockHeight") 
+                        REFERENCES "bridge_block_entity" ("height") ON DELETE CASCADE ON UPDATE NO ACTION,
+                    CONSTRAINT "FK_e43db92f2466f82759cec509625" FOREIGN KEY ("spendBlockHeight") 
+                        REFERENCES "bridge_block_entity" ("height") ON DELETE SET NULL ON UPDATE NO ACTION
+                )`
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "observed_commitment_entity" RENAME TO "temporary_observed_commitment_entity"`);
-        await queryRunner.query(`CREATE TABLE "observed_commitment_entity" ("id" integer PRIMARY KEY NOT NULL, "eventId" varchar NOT NULL, "commitment" varchar NOT NULL, "WID" varchar NOT NULL, "commitmentBoxId" varchar NOT NULL, "eventTriggerBoxId" varchar, "blockHeight" integer, "spendBlockHeight" integer)`);
-        await queryRunner.query(`INSERT INTO "observed_commitment_entity"("id", "eventId", "commitment", "WID", "commitmentBoxId", "eventTriggerBoxId", "blockHeight", "spendBlockHeight") SELECT "id", "eventId", "commitment", "WID", "commitmentBoxId", "eventTriggerBoxId", "blockHeight", "spendBlockHeight" FROM "temporary_observed_commitment_entity"`);
-        await queryRunner.query(`DROP TABLE "temporary_observed_commitment_entity"`);
-        await queryRunner.query(`ALTER TABLE "box_entity" RENAME TO "temporary_box_entity"`);
-        await queryRunner.query(`CREATE TABLE "box_entity" ("id" integer PRIMARY KEY NOT NULL, "boxId" varchar NOT NULL, "value" bigint NOT NULL, "type" varchar CHECK( "type" IN ('permit','wid','plain') ) NOT NULL, "boxJson" varchar NOT NULL, "blockHeight" integer, "spendBlockHeight" integer)`);
-        await queryRunner.query(`INSERT INTO "box_entity"("id", "boxId", "value", "type", "boxJson", "blockHeight", "spendBlockHeight") SELECT "id", "boxId", "value", "type", "boxJson", "blockHeight", "spendBlockHeight" FROM "temporary_box_entity"`);
-        await queryRunner.query(`DROP TABLE "temporary_box_entity"`);
         await queryRunner.query(`DROP TABLE "observed_commitment_entity"`);
         await queryRunner.query(`DROP TABLE "box_entity"`);
         await queryRunner.query(`DROP TABLE "bridge_block_entity"`);
