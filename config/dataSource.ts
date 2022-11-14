29c10950085358d3d70483646230f967e4c89a62
Mohammad Kermani <mkermani144@gmail.com>
2022-11-14 12:30:50 +0330

    Fix issue in `typeorm:generate` npm script
    When `typeorm:generate` is run, it uses `ts-node` internally. By default,
    if we try to import modules without extension for file paths, `ts-node`
    raises errors because of missing extensions. To prevent this, we need to
    provide an experimental flag to `ts-node` like we have done for `start`
    npm script.
    
    Fix the error by providing the flag to `typeorm-ts-node-esm` in `typeorm`
    npm script, which is used by `typeorm:generate`.

--- config/dataSource.ts
+++ config/dataSource.ts
@@ -2,23 +2,35 @@ import path from 'path';
 import { DataSource } from 'typeorm';
 import { fileURLToPath } from 'url';
 
+import { BoxEntity } from '@rosen-bridge/address-extractor';
+import { ObservationEntity } from '@rosen-bridge/observation-extractor';
+import { BlockEntity } from '@rosen-bridge/scanner';
+import {
+  CommitmentEntity,
+  EventTriggerEntity,
+  PermitEntity,
+} from '@rosen-bridge/watcher-data-extractor';
+
+import { ObservationStatusEntity } from '../src/database/entities/observationStatusEntity';
+import { TxEntity } from '../src/database/entities/txEntity';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// TODO: datasource config
-//  fix entities directories
-//  fix migrations (use package migrations)
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: __dirname + '/../sqlite/watcher.sqlite',
   entities: [
-    'src/database/entities/*.ts',
-    'node_modules/@rosen-bridge/scanner/dist/entities/*.js',
-    'node_modules/@rosen-bridge/watcher-data-extractor/dist/entities/*.js',
-    'node_modules/@rosen-bridge/observation-extractor/dist/entities/*.js',
-    'node_modules/@rosen-bridge/address-extractor/dist/entities/*.js',
+    BlockEntity,
+    BoxEntity,
+    CommitmentEntity,
+    EventTriggerEntity,
+    ObservationEntity,
+    ObservationStatusEntity,
+    PermitEntity,
+    TxEntity,
   ],
-  migrations: ['src/database/migrations/watcher/*.ts'],
+  migrations: ['../src/database/migrations/watcher/*.ts'],
   synchronize: false,
   logging: false,
 });
