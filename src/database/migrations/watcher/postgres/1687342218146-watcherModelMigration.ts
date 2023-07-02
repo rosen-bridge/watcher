c09fa62a489ec9c463bdc2b3f0a3f7c575ecc78b
fateme.r <fatemeh.r3366@gmail.com>
2023-07-02 11:54:28 +0330

    Add view migrations

--- src/database/migrations/watcher/postgres/1687342218146-watcherModelMigration.ts
+++ src/database/migrations/watcher/postgres/1687342218146-watcherModelMigration.ts
@@ -5,22 +5,29 @@ export class watcherModelMigration1687342218146 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE VIEW "revenue_view" AS
-                SELECT pe.id, pe."txId" "permitTxId", ete."eventId", 
-                ete.height "lockHeight", ete."fromChain", ete."toChain", ete."fromAddress",
-                ete."toAddress", ete."amount", ete."bridgeFee", ete."networkFee",
-                ete."sourceChainTokenId" "tokenId", ete."sourceTxId" "lockTxId", 
-                be.height, be.timestamp,
-                CASE
-                  WHEN "ete"."spendTxId" IS NULL THEN 'Doing'
-                  ELSE 'Done'
-                END AS "status",
-                re."tokenId" "revenueTokenId", re.amount "revenueAmount"
-                FROM permit_entity pe
-                LEFT JOIN event_trigger_entity ete ON pe."txId" = ete."txId"
-                LEFT JOIN block_entity be ON pe.block = be.hash
-                LEFT JOIN revenue_entity re ON pe.id = re."permitId";
-        `);
+      CREATE VIEW "revenue_view" AS
+      SELECT "pe"."id" AS "id",
+          pe."txId" AS "permitTxId",
+          ete."eventId" AS "eventId",
+          ete."sourceChainHeight" AS "lockHeight",
+          ete."fromChain" AS "fromChain",
+          ete."toChain" AS "toChain",
+          ete."fromAddress" AS "fromAddress",
+          ete."toAddress" AS "toAddress",
+          ete."amount" AS "amount",
+          ete."bridgeFee" AS "bridgeFee",
+          ete."networkFee" AS "networkFee",
+          ete."sourceChainTokenId" AS "tokenId",
+          ete."sourceTxId" AS "lockTxId",
+          pe."height" AS "height",
+          be."timestamp" AS "timestamp",
+          re."tokenId" AS "revenueTokenId",
+          re."amount" AS "revenueAmount"
+      FROM "permit_entity" "pe"
+          LEFT JOIN "event_trigger_entity" "ete" ON pe."txId" = ete."spendTxId"
+          LEFT JOIN "block_entity" "be" ON "pe"."block" = "be"."hash"
+          LEFT JOIN "revenue_entity" "re" ON "pe"."id" = re."permitId"
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
