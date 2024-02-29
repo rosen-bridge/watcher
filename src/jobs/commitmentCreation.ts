fe8aeccc531d9bdfc18ae57698b8d0faea59ed68
fateme.r <fatemeh.r3366@gmail.com>
2024-02-29 12:37:55 +0330

    Add ergo scanner sync constraint on operations

--- src/jobs/commitmentCreation.ts
+++ src/jobs/commitmentCreation.ts
@@ -1,27 +1,22 @@
-import {
-  ErgoNodeScannerHealthCheck,
-  HealthStatusLevel,
-} from '@rosen-bridge/health-check';
+import { HealthStatusLevel } from '@rosen-bridge/health-check';
 import WinstonLogger from '@rosen-bridge/winston-logger';
-import { dataSource } from '../../config/dataSource';
 import { getConfig } from '../config/config';
 import { Boxes } from '../ergo/boxes';
 import { CommitmentCreation } from '../transactions/commitmentCreation';
-import { scanner } from '../utils/scanner';
 import { TransactionUtils, WatcherUtils } from '../utils/watcherUtils';
 import { redeemJob } from './commitmentRedeem';
+import { HealthCheckSingleton } from '../utils/healthCheck';
 
 const logger = WinstonLogger.getInstance().getLogger(import.meta.url);
 
 let commitmentCreatorObj: CommitmentCreation;
 let redeemExecuted = false;
-let ergoScannerSyncCheck: ErgoNodeScannerHealthCheck;
 
 const creationJob = async () => {
   try {
-    await ergoScannerSyncCheck.update();
-    const status = await ergoScannerSyncCheck.getHealthStatus();
-    if (status === HealthStatusLevel.HEALTHY) {
+    const scannerSyncStatus =
+      await HealthCheckSingleton.getInstance().getErgoScannerSyncHealth();
+    if (scannerSyncStatus === HealthStatusLevel.HEALTHY) {
       await commitmentCreatorObj.job();
       if (!redeemExecuted) {
         redeemExecuted = true;
@@ -43,12 +38,5 @@ export const creation = (
   boxes: Boxes
 ) => {
   commitmentCreatorObj = new CommitmentCreation(watcherUtils, txUtils, boxes);
-  ergoScannerSyncCheck = new ErgoNodeScannerHealthCheck(
-    dataSource,
-    scanner.ergoScanner.name(),
-    getConfig().healthCheck.ergoScannerWarnDiff,
-    getConfig().healthCheck.ergoScannerCriticalDiff,
-    getConfig().general.nodeUrl
-  );
   creationJob();
 };
--- src/jobs/commitmentReveal.ts
+++ src/jobs/commitmentReveal.ts
@@ -1,25 +1,20 @@
-import {
-  ErgoNodeScannerHealthCheck,
-  HealthStatusLevel,
-} from '@rosen-bridge/health-check';
+import { HealthStatusLevel } from '@rosen-bridge/health-check';
 import WinstonLogger from '@rosen-bridge/winston-logger';
-import { dataSource } from '../../config/dataSource';
 import { getConfig } from '../config/config';
 import { Boxes } from '../ergo/boxes';
 import { CommitmentReveal } from '../transactions/commitmentReveal';
-import { scanner } from '../utils/scanner';
 import { TransactionUtils, WatcherUtils } from '../utils/watcherUtils';
+import { HealthCheckSingleton } from '../utils/healthCheck';
 
 const logger = WinstonLogger.getInstance().getLogger(import.meta.url);
 
 let commitmentRevealingObj: CommitmentReveal;
-let ergoScannerSyncCheck: ErgoNodeScannerHealthCheck;
 
 const revealJob = async () => {
   try {
-    await ergoScannerSyncCheck.update();
-    const status = await ergoScannerSyncCheck.getHealthStatus();
-    if (status === HealthStatusLevel.HEALTHY) {
+    const scannerSyncStatus =
+      await HealthCheckSingleton.getInstance().getErgoScannerSyncHealth();
+    if (scannerSyncStatus === HealthStatusLevel.HEALTHY) {
       await commitmentRevealingObj.job();
     }
   } catch (e) {
@@ -34,12 +29,5 @@ export const reveal = (
   boxes: Boxes
 ) => {
   commitmentRevealingObj = new CommitmentReveal(watcherUtils, txUtils, boxes);
-  ergoScannerSyncCheck = new ErgoNodeScannerHealthCheck(
-    dataSource,
-    scanner.ergoScanner.name(),
-    getConfig().healthCheck.ergoScannerWarnDiff,
-    getConfig().healthCheck.ergoScannerCriticalDiff,
-    getConfig().general.nodeUrl
-  );
   revealJob();
 };
