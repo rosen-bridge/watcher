ad68a560c48adad1cdb70931b276fd9517ec2c12
fateme.r <fatemeh.r3366@gmail.com>
2024-08-13 10:50:08 +0330

    fix init scanner network watcher switch

--- src/jobs/initScanner.ts
+++ src/jobs/initScanner.ts
@@ -12,6 +12,14 @@ import { scanner } from '../utils/scanner';
 import WinstonLogger from '@rosen-bridge/winston-logger';
 import { EvmRpcScanner } from '@rosen-bridge/evm-rpc-scanner';
 
+const allConfig = getConfig();
+const {
+  general: config,
+  cardano: cardanoConfig,
+  bitcoin: bitcoinConfig,
+  ethereum: ethereumConfig,
+} = allConfig;
+
 const logger = WinstonLogger.getInstance().getLogger(import.meta.url);
 
 const scanningJob = async (interval: number, scanner: GeneralScanner<any>) => {
@@ -26,49 +34,50 @@ const scanningJob = async (interval: number, scanner: GeneralScanner<any>) => {
 };
 
 export const scannerInit = () => {
-  const allConfig = getConfig();
-  const config = allConfig.general;
   scanningJob(config.ergoInterval, scanner.ergoScanner).then(() => null);
-  if (config.networkWatcher === Constants.CARDANO_CHAIN_NAME) {
-    const cardanoConfig = allConfig.cardano;
-    if (cardanoConfig.ogmios)
-      (scanner.observationScanner as CardanoOgmiosScanner).start();
-    else if (cardanoConfig.koios) {
-      scanningJob(
-        cardanoConfig.koios.interval,
-        scanner.observationScanner as CardanoKoiosScanner
-      ).then(() => null);
-    } else if (cardanoConfig.blockfrost) {
-      scanningJob(
-        cardanoConfig.blockfrost.interval,
-        scanner.observationScanner as CardanoBlockFrostScanner
-      ).then(() => null);
-    } else throw new Error(`Cardano scanner is not configured properly`);
-  } else if (config.networkWatcher === Constants.BITCOIN_CHAIN_NAME) {
-    const bitcoinConfig = allConfig.bitcoin;
-    if (bitcoinConfig.esplora) {
-      scanningJob(
-        bitcoinConfig.esplora.interval,
-        scanner.observationScanner as BitcoinEsploraScanner
-      ).then(() => null);
-    } else if (bitcoinConfig.rpc) {
-      scanningJob(
-        bitcoinConfig.rpc.interval,
-        scanner.observationScanner as BitcoinRpcScanner
-      ).then(() => null);
-    } else throw new Error(`Bitcoin scanner is not configured properly`);
-  } else if (config.networkWatcher === Constants.ETHEREUM_CHAIN_NAME) {
-    const ethereumConfig = allConfig.ethereum;
-    if (ethereumConfig.rpc) {
-      scanningJob(
-        ethereumConfig.rpc.interval,
-        scanner.observationScanner as EvmRpcScanner
-      ).then(() => null);
-    } else throw new Error(`Ethereum scanner is not configured properly`);
-  } else {
-    throw new Error(
-      `The observing network [${config.networkWatcher}] is not supported`
-    );
+  switch (config.networkWatcher) {
+    case Constants.CARDANO_CHAIN_NAME:
+      if (cardanoConfig.ogmios)
+        (scanner.observationScanner as CardanoOgmiosScanner).start();
+      else if (cardanoConfig.koios) {
+        scanningJob(
+          cardanoConfig.koios.interval,
+          scanner.observationScanner as CardanoKoiosScanner
+        ).then(() => null);
+      } else if (cardanoConfig.blockfrost) {
+        scanningJob(
+          cardanoConfig.blockfrost.interval,
+          scanner.observationScanner as CardanoBlockFrostScanner
+        ).then(() => null);
+      } else throw new Error(`Cardano scanner is not configured properly`);
+      break;
+    case Constants.BITCOIN_CHAIN_NAME:
+      if (bitcoinConfig.esplora) {
+        scanningJob(
+          bitcoinConfig.esplora.interval,
+          scanner.observationScanner as BitcoinEsploraScanner
+        ).then(() => null);
+      } else if (bitcoinConfig.rpc) {
+        scanningJob(
+          bitcoinConfig.rpc.interval,
+          scanner.observationScanner as BitcoinRpcScanner
+        ).then(() => null);
+      } else throw new Error(`Bitcoin scanner is not configured properly`);
+      break;
+    case Constants.ETHEREUM_CHAIN_NAME:
+      if (ethereumConfig.rpc) {
+        scanningJob(
+          ethereumConfig.rpc.interval,
+          scanner.observationScanner as EvmRpcScanner
+        ).then(() => null);
+      } else throw new Error(`Ethereum scanner is not configured properly`);
+      break;
+    case Constants.ERGO_CHAIN_NAME:
+      break;
+    default:
+      throw new Error(
+        `The observing network [${config.networkWatcher}] is not supported`
+      );
   }
 
   // TODO: Add commitment cleanup job
