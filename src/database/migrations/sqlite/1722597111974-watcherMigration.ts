0207b9c44a081f32e91351df774aa3070e1fc074
fateme.r <fatemeh.r3366@gmail.com>
2024-09-07 15:42:29 +0330

    Merge branch 'fix-ogmios-connection' into 'dev'
    update scanner packages
    
    See merge request ergo/rosen-bridge/watcher!285

---
+++ src/database/migrations/sqlite/1722597111974-watcherMigration.ts
@@ -0,0 +1,103 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class WatcherMigration1722597111974 implements MigrationInterface {
+  name = 'WatcherMigration1722597111974';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `
+          DELETE FROM "typeorm_metadata"
+          WHERE "type" = ?
+              AND "name" = ?
+      `,
+      ['VIEW', 'revenue_chart_data']
+    );
+    await queryRunner.query(`
+        DROP VIEW "revenue_chart_data"
+    `);
+    await queryRunner.query(
+      `
+          DELETE FROM "typeorm_metadata"
+          WHERE "type" = ?
+              AND "name" = ?
+      `,
+      ['VIEW', 'revenue_view']
+    );
+    await queryRunner.query(`
+        DROP VIEW "revenue_view"
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+    CREATE VIEW "revenue_view" AS
+    SELECT "pe"."id" AS "id",
+        "pe"."WID" AS "wid",
+        "ete"."sourceChainHeight" AS "lockHeight",
+        "be"."height" AS "height",
+        "be"."timestamp" AS "timestamp",
+        pe."txId" AS "permitTxId",
+        ete."eventId" AS "eventId",
+        ete."fromChain" AS "fromChain",
+        ete."toChain" AS "toChain",
+        ete."fromAddress" AS "fromAddress",
+        ete."toAddress" AS "toAddress",
+        ete."amount" AS "amount",
+        ete."bridgeFee" AS "bridgeFee",
+        ete."networkFee" AS "networkFee",
+        ete."sourceChainTokenId" AS "tokenId",
+        ete."sourceTxId" AS "lockTxId"
+    FROM "permit_entity" "pe"
+        INNER JOIN "event_trigger_entity" "ete" ON pe."txId" = ete."spendTxId"
+        LEFT JOIN "block_entity" "be" ON "pe"."block" = "be"."hash"
+    `);
+    await queryRunner.query(
+      `
+          INSERT INTO "typeorm_metadata"(
+                  "database",
+                  "schema",
+                  "table",
+                  "type",
+                  "name",
+                  "value"
+              )
+          VALUES (NULL, NULL, NULL, ?, ?, ?)
+      `,
+      [
+        'VIEW',
+        'revenue_view',
+        'SELECT "pe"."id" AS "id", "pe"."WID" AS "wid", "ete"."sourceChainHeight" AS "lockHeight", "be"."height" AS "height", "be"."timestamp" AS "timestamp", pe."txId" AS "permitTxId", ete."eventId" AS "eventId", ete."fromChain" AS "fromChain", ete."toChain" AS "toChain", ete."fromAddress" AS "fromAddress", ete."toAddress" AS "toAddress", ete."amount" AS "amount", ete."bridgeFee" AS "bridgeFee", ete."networkFee" AS "networkFee", ete."sourceChainTokenId" AS "tokenId", ete."sourceTxId" AS "lockTxId" FROM "permit_entity" "pe" INNER JOIN "event_trigger_entity" "ete" ON pe."txId" = ete."spendTxId"  LEFT JOIN "block_entity" "be" ON "pe"."block" = "be"."hash"',
+      ]
+    );
+    await queryRunner.query(`
+      CREATE VIEW "revenue_chart_data" AS
+      SELECT "be"."year" AS "year",
+          "be"."month" AS "month",
+          "be"."day" AS "day",
+          re."tokenId" AS "tokenId",
+          re."amount" AS "amount",
+          be."timestamp" AS "timestamp"
+      FROM "revenue_entity" "re"
+          INNER JOIN "permit_entity" "pe" ON re."permitId" = "pe"."id"
+          INNER JOIN "block_entity" "be" ON "pe"."block" = "be"."hash"
+    `);
+    await queryRunner.query(
+      `
+      INSERT INTO "typeorm_metadata"(
+              "database",
+              "schema",
+              "table",
+              "type",
+              "name",
+              "value"
+          )
+      VALUES (NULL, NULL, NULL, ?, ?, ?)
+      `,
+      [
+        'VIEW',
+        'revenue_chart_data',
+        'SELECT "be"."year" AS "year", "be"."month" AS "month", "be"."day" AS "day", re."tokenId" AS "tokenId", re."amount" AS "amount", be."timestamp" AS "timestamp" FROM "revenue_entity" "re" INNER JOIN "permit_entity" "pe" ON re."permitId" = "pe"."id"  INNER JOIN "block_entity" "be" ON "pe"."block" = "be"."hash"',
+      ]
+    );
+  }
+}
