6b72f65382f00bf5dd41d1efa8b02169bb03f3b3
fateme.r <fatemeh.r3366@gmail.com>
2025-05-31 15:54:33 +0330

    Add doge rate limit configuration

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
 import WinstonLogger from '@rosen-bridge/winston-logger';
-import { getConfig } from './config/config';
-import packageJson from '../package.json' assert { type: 'json' };
 import { CallbackLoggerFactory } from '@rosen-bridge/callback-logger';
 import axios from '@rosen-bridge/rate-limited-axios';
+
+import { getConfig } from './config/config';
+import packageJson from '../package.json' assert { type: 'json' };
+import { DOGE_CHAIN_NAME } from './config/constants';
 
 CallbackLoggerFactory.init(new WinstonLogger(getConfig().logger.transports));
 const logger = CallbackLoggerFactory.getInstance().getLogger(import.meta.url);
@@ -11,12 +13,20 @@ const logger = CallbackLoggerFactory.getInstance().getLogger(import.meta.url);
 logger.info(`Watcher version: ${packageJson.version}`);
 logger.info(`Watcher contract version: ${getConfig().rosen.contractVersion}`);
 
+let limitRules: { pattern: string; rateLimit: number }[] = [];
+if (getConfig().general.networkWatcher === DOGE_CHAIN_NAME && getConfig().doge.rpc.length > 0) {
+limitRules = getConfig().doge.rpc.map((rpcConfig) => {
+  return {
+    pattern: `^${rpcConfig.url}$`,
+    rateLimit: 3,
+  };
+});
+}
+
 axios.initConfigs(
   {
-    apiLimitRateRangeAsSeconds: 10,
-    apiLimitRules: [
-      // { pattern: '.*/info', rateLimit: 1 },
-    ],
+    apiLimitRateRangeAsSeconds: 1,
+    apiLimitRules: limitRules,
   },
   logger
 );
