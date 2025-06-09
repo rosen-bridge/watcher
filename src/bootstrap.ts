f6d286617526b93af8d393f1fae19060dd7d1d93
fateme.r <fatemeh.r3366@gmail.com>
2025-06-09 16:10:44 +0330

    update rate limited axios and its configuration

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import WinstonLogger from '@rosen-bridge/winston-logger';
 import { CallbackLoggerFactory } from '@rosen-bridge/callback-logger';
-import axios from '@rosen-bridge/rate-limited-axios';
+import { RateLimitedAxiosConfig } from '@rosen-bridge/rate-limited-axios';
 
 import { getConfig } from './config/config';
 import packageJson from '../package.json' assert { type: 'json' };
@@ -13,20 +13,10 @@ const logger = CallbackLoggerFactory.getInstance().getLogger(import.meta.url);
 logger.info(`Watcher version: ${packageJson.version}`);
 logger.info(`Watcher contract version: ${getConfig().rosen.contractVersion}`);
 
-let limitRules: { pattern: string; rateLimit: number }[] = [];
 if (getConfig().general.networkWatcher === DOGE_CHAIN_NAME && getConfig().doge.rpc.length > 0) {
-limitRules = getConfig().doge.rpc.map((rpcConfig) => {
-  return {
-    pattern: `^${rpcConfig.url}$`,
-    rateLimit: 3,
-  };
-});
+  getConfig().doge.rpc.forEach((rpcConfig) => {
+    RateLimitedAxiosConfig.addRule(`^${rpcConfig.url}$`, 3, 1);
+  });
 }
 
-axios.initConfigs(
-  {
-    apiLimitRateRangeAsSeconds: 1,
-    apiLimitRules: limitRules,
-  },
-  logger
-);
+RateLimitedAxiosConfig.setLogger(logger);
